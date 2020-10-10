7da2c2dcc4c4700095e5687d4903bed48f27bde7
Volha Soika <Volha_Soika@epam.com>
2020-10-10 22:54:02 +0300

    feat: install routing, add NoFound page

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,32 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import App from "./components/App.tsx";
+import NoMatch from "./components/404page";
 import ErrorBoundary from "./components/ErrorBoundary";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
+        </Route>
+        <Route path="/404" component={NoMatch} />
+        <Route path="*">
+          <Redirect to="/404" />
+        </Route>
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
